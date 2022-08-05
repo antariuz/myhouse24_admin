b5d45761dea3a825aa7d1e088bba50870c9bd04c
black <antariuz@gmail.com>
2022-08-05 10:30:11 +0300

    Initial commit

---
+++ src/main/java/avada/media/myhouse24_admin/Myhouse24AdminApplication.java
@@ -0,0 +1,13 @@
+package avada.media.myhouse24_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myhouse24AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myhouse24AdminApplication.class, args);
+	}
+
+}
