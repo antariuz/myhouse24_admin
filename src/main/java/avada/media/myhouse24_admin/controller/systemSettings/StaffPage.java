81ae6f379f749a07a00a62ea9f115230f122edcc
black <antariuz@gmail.com>
2022-08-24 17:07:05 +0300

    Web Part of Staff Page is done

--- src/main/java/avada/media/myhouse24_admin/controller/systemSettings/StaffPage.java
+++ src/main/java/avada/media/myhouse24_admin/controller/systemSettings/StaffPage.java
@@ -1,6 +1,6 @@
 package avada.media.myhouse24_admin.controller.systemSettings;
 
-import avada.media.myhouse24_admin.model.User;
+import avada.media.myhouse24_admin.model.Staff;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +11,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("system-settings/users")
-public class UsersPage {
+@RequestMapping("system-settings/staff")
+public class StaffPage {
 
     @RequestMapping({"/", ""})
-    public ModelAndView showUsersPage() {
-        return new ModelAndView("pages/system-settings/users");
+    public ModelAndView showStaffPage() {
+        return new ModelAndView("pages/system-settings/staff");
     }
 
     @GetMapping("get-all")
-    public @ResponseBody List<User> getAllUsers() {
+    public @ResponseBody List<Staff> getAllStaff() {
         return new ArrayList<>();
     }
 
